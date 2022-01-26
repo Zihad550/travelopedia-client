981ace941ca2848be78e276bf45a550bd23208ca
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-26 09:11:56 +0600

    blog card completed

--- src/Pages/Shared/Blogs/Blogs/Blogs.js
+++ src/Pages/Shared/Blogs/Blogs/Blogs.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("./blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
   return (
-    <div>
+    <div className="">
       <h2 className="text-center text-5xl my-12 bg-slate-400 py-6 ">Blogs</h2>
+      <div className="grid md:mx-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {blogs.map((blog) => (
+          <Blog blog={blog} key={blog.id} />
+        ))}
+      </div>
     </div>
   );
 };
